c5034ff4189a4e878994a5f5c709f06714c2d269
Bronte Baer <bbaer@ischool.berkeley.edu>
2023-04-02 18:49:10 -0600

    BB trying exact chart Bri made

--- src/components/data_viz/BBPlotlyComponentPractice.jsx
+++ src/components/data_viz/BBPlotlyComponentPractice.jsx
@@ -2,31 +2,44 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 
 function BBPlotlyComponentPractice(props) {
-    const AreaChart = (props) => {
-        var data = [
-            {
-                x: [1, 2, 3, 4],
-                y: [0, 2, 3, 5],
-                fill: 'tozeroy',
-                type: 'scatter',
-                name: 'Vendor'
-            },
-            {
-                x: [1, 2, 3, 4],
-                y: [3, 5, 1, 7],
-                fill: 'tonexty',
-                type: 'scatter',
-                name: 'Provider'
-            }
-        ];
-
-        return (
-            <Plot
-                data={data}
-                layout={{ width: 500, height: 500, title: 'Area Chart' }} />
-        )
+    const linspaceFn = (startValue, stopValue, cardinality) => {
+        var arr = [];
+        var step = (stopValue - startValue) / (cardinality - 1);
+        for (var i = 0; i < cardinality; i++) {
+            arr.push(parseFloat((startValue + (step * i)).toFixed(3)));
+        }
+        return arr;
     }
+    const t = linspaceFn(0, 20, 100)
+    const x = t.map(i => (Math.cos(i)))
+    const y = t.map(i => Math.sin(i))
+    const z = t
+
+    return (
+        <div>
+            <Plot
+                onClick={(data) => {
+                    console.log(data.points[0])
+                }}
+
+                data={[
+                    {
+                        x: x,
+                        y: y,
+                        z: z,
+                        mode: 'markers',
+                        type: 'scatter3d',
+                        marker: {
+                            size: 12,
+                            color: z,
+                            colorscale: 'Viridis',
+                            opacity: 0.8
+                        }
+                    }
+                ]}
+            />
+        </div>
+    );
 }
 
 export default BBPlotlyComponentPractice;
-
