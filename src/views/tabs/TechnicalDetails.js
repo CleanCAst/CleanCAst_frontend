39754a12844ce2a4ddc05e1cb740cc7e1a645026
Bronte Baer <bbaer@ischool.berkeley.edu>
2023-04-13 19:40:27 -0600

    BB testing tom's image

--- src/views/tabs/TechnicalDetails.js
+++ src/views/tabs/TechnicalDetails.js
@@ -17,6 +17,12 @@ export default function TechnicalDetails() {
         <>
             <div className="section">
                 <Container>
+                    <h2>Testing Tom's double slider viz</h2>
+                    <iframe width="1000" height="900" frameborder="0" src="https://ucb-mids-capstone.s3.us-west-2.amazonaws.com/Vizzes/carbon_intensity_and_source_double_axis_single_slider_plot.html"></iframe>
+                </Container>
+            </div>
+            <div className="section">
+                <Container>
                     <h2>Testing public url for html data viz files</h2>
                     <iframe width="800" height="600" frameborder="0" src="https://ucb-mids-capstone.s3.us-west-2.amazonaws.com/Vizzes/net_generation_and_co2_forecasts_viz.html"></iframe>
                 </Container>
